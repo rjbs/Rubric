932349a42318c47e02cbafb53b6d2c6968eb3f20
Ricardo SIGNES <rjbs@codesimply.com>
2006-06-04 01:01:08 +0000

    r22057@knight:  rjbs | 2006-06-03 21:00:45 -0400   add linkchecker

---
+++ lib/Rubric/CLI/Command/linkcheck.pm
@@ -0,0 +1,48 @@
+package Rubric::CLI::Command::linkcheck;
+use base qw(Rubric::CLI::Command);
+
+=head1 NAME
+
+Rubric::CLI::Command::linkcheck - check validity of links in the database
+
+=head1 VERSION
+
+ $Id: /my/cs/projects/rubric/trunk/lib/Rubric/CLI/Command/db.pm 21614 2006-05-23T16:33:47.947111Z jcap  $
+
+=cut
+
+use strict;
+use warnings;
+
+use LWP::Simple ();
+use Rubric::DBI::Setup;
+
+sub describe_options {
+  my ($opt, $usage) = Getopt::Long::Descriptive::describe_options(
+    "rubric linkcheck %o",
+    # options...
+  );
+
+  return ($opt, $usage);
+}
+
+sub execute {
+  my ($class) = @_;
+  my ($opt, $usage) = $class->describe_options;
+
+  my $links = Rubric::Link->retrieve_all;
+
+  while (my $link = $links->next) {
+    my $uri = $link->uri;
+    if ($uri->scheme ne 'http') {
+      print "unknown scheme on link $link\n";
+      next;
+    }
+
+    unless (LWP::Simple::head($uri)) {
+      print "couldn't get headers for $uri\n";
+    }
+  }
+}
+
+1;
