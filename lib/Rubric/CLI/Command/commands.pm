e405fab8240e435d59a1c29b3bf16f315bfbcf64
Ricardo SIGNES <rjbs@codesimply.com>
2006-06-12 03:40:57 +0000

    r22401@knight:  rjbs | 2006-06-11 22:42:27 -0400  strip out more appcmd stuff

--- lib/Rubric/CLI/Command/commands.pm
+++ lib/Rubric/CLI/Command/commands.pm
@@ -13,40 +13,6 @@ Rubric::CLI::Command::commands - list the rubric commands
 use strict;
 use warnings;
 
-use base qw(Rubric::CLI::Command);
-
-# stolen from ExtUtils::MakeMaker
-sub _parse_abstract {
-  my ($module) = @_;
-  my $result;
-
-  (my $pm_file = $module) =~ s!::!/!g;
-  $pm_file .= '.pm';
-  $pm_file = $INC{$pm_file};
-  open my $fh, "<", $pm_file or return "(unknown)";
-
-  local $/ = "\n";
-  my $inpod = 0;
-  while (<$fh>) {
-    $inpod = /^=(?!cut)/ ? 1
-           : /^=cut/     ? 0
-           :               $inpod;
-    next unless $inpod;
-    chomp;
-    next unless /^($module\s-\s)(.*)/;
-    $result = $2;
-    last;
-  }
-  return $result || "(unknown)";
-} 
-
-sub run {
-  my ($self) = @_;
-
-  for my $command (sort $self->app->commands) {
-    my $abstract = _parse_abstract($self->app->plugin_for($command));
-    printf "%10s: %s\n", $command, $abstract;
-  }
-}
+use base qw(App::Cmd::Command::commands);
 
 1;
